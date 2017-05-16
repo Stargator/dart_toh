c6302dac345c6c5410e0b2af6a203bf2109f73b7
Stargator <Stargator@users.noreply.github.com>
2017-05-15 21:31:27 -0400

    Part 1 of Tutorial with tests

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -4,9 +4,35 @@
 import 'package:angular2/angular2.dart';
 
 @Component(
-  selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
-)
+    selector: 'my-app',
+    directives: const [COMMON_DIRECTIVES], // ignore: always_specify_types
+    template: '''
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    ''')
+
+/// AppComponent class
+/// Container of all components of the app
 class AppComponent {
-  var name = 'Angular';
+  /// Title of the app
+  String title = "Tour of Heroes";
+
+  /// Temporary hero for the demo
+  Hero hero = new Hero(1, "Windstorm");
+}
+
+/// Class that defines the attributes of heroes
+class Hero {
+  /// Unique Id
+  final int id;
+  /// Hero's given name
+  String name;
+
+  /// Only constructor with parameters
+  Hero(this.id, this.name);
 }
