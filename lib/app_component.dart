2393e4b16b4ad0ad98fce3b4b6e83092fb8fd826
Stargator <Stargator@users.noreply.github.com>
2017-05-28 13:24:37 -0400

    Part 3 of Tutorial - Services

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,10 +1,12 @@
 // Copyright (c) 2017. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'dart:async';
 import 'package:angular2/angular2.dart';
 import 'package:angular2/core.dart'; // Can be removed?
 import 'hero.dart';
 import 'hero_detail_component.dart';
+import 'hero_service.dart';
 
 @Component(
     selector: 'my-app',
@@ -20,79 +22,79 @@ import 'hero_detail_component.dart';
 
     <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     ''',
-    directives:
-    const [COMMON_DIRECTIVES, HeroDetailComponent],
-  styles: const [
-    '''
-      .selected {
-        background-color: #CFD8DC !important;
-        color: white;
-      }
-      .heroes {
-        margin: 0 0 2em 0;
-        list-style-type: none;
-        padding: 0;
-        width: 15em;
-      }
-      .heroes li {
-        cursor: pointer;
-        position: relative;
-        left: 0;
-        background-color: #EEE;
-        margin: .5em;
-        padding: .3em 0em;
-        height: 1.6em;
-        border-radius: 4px;
-      }
-      .heroes li.selected:hover {
-        color: white;
-      }
-      .heroes li:hover {
-        color: #607D8B;
-        background-color: #EEE;
-        left: .1em;
-      }
-      .heroes .text {
-        position: relative;
-        top: -3px;
-      }
-      .heroes .badge {
-        display: inline-block;
-        font-size: small;
-        color: white;
-        padding: 0.8em 0.7em 0em 0.7em;
-        background-color: #607D8B;
-        line-height: 1em;
-        position: relative;
-        left: -1px;
-        top: -4px;
-        height: 1.8em;
-        margin-right: .8em;
-        border-radius: 4px 0px 0px 4px;
-      }
-    '''
+    directives: const [COMMON_DIRECTIVES, HeroDetailComponent],
+    providers: const [HeroService],
+    styles: const [
+      '''
+        .selected {
+          background-color: #CFD8DC !important;
+          color: white;
+        }
+        .heroes {
+          margin: 0 0 2em 0;
+          list-style-type: none;
+          padding: 0;
+          width: 15em;
+        }
+        .heroes li {
+          cursor: pointer;
+          position: relative;
+          left: 0;
+          background-color: #EEE;
+          margin: .5em;
+          padding: .3em 0em;
+          height: 1.6em;
+          border-radius: 4px;
+        }
+        .heroes li.selected:hover {
+          color: white;
+        }
+        .heroes li:hover {
+          color: #607D8B;
+          background-color: #EEE;
+          left: .1em;
+        }
+        .heroes .text {
+          position: relative;
+          top: -3px;
+        }
+        .heroes .badge {
+          display: inline-block;
+          font-size: small;
+          color: white;
+          padding: 0.8em 0.7em 0em 0.7em;
+          background-color: #607D8B;
+          line-height: 1em;
+          position: relative;
+          left: -1px;
+          top: -4px;
+          height: 1.8em;
+          margin-right: .8em;
+          border-radius: 4px 0px 0px 4px;
+        }
+      '''
   ]
 )
 
-class AppComponent {
+class AppComponent implements OnInit {
   final String title = "Tour of Heroes";
-  final List<Hero> heroes = mockHeroes;
+  final HeroService _heroService;
+
+  AppComponent(this._heroService);
+  List<Hero> heroes;
   Hero selectedHero;
+
+  Future<Null> getHeroes() async {
+    heroes = await _heroService.getHeroes();
+  }
 
   void onSelect(Hero hero) {
     selectedHero = hero;
   }
+
+  @override
+  ngOnInit() {
+    getHeroes();
+  }
 }
 
-final mockHeroes = <Hero>[
-  new Hero(11, 'Mr. Nice'),
-  new Hero(12, 'Narco'),
-  new Hero(13, 'Bombasto'),
-  new Hero(14, 'Celeritas'),
-  new Hero(15, 'Magneta'),
-  new Hero(16, 'RubberMan'),
-  new Hero(17, 'Dynama'),
-  new Hero(18, 'Dr IQ'),
-  new Hero(19, 'Magma'),
-  new Hero(20, 'Tornado')
-];
