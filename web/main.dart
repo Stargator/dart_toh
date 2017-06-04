16df6d36f94c2c5328534ecf218a93086fe45bbd
Stargator <Stargator@users.noreply.github.com>
2017-06-04 19:35:27 -0400

    Part 6 of Tutorial - HTTP - Before Simulate the web API

--- web/main.dart
+++ web/main.dart
@@ -1,10 +1,14 @@
 // Copyright (c) 2017. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'package:angular2/angular2.dart';
 import 'package:angular2/platform/browser.dart';
+import 'package:http/browser_client.dart';
 
 import 'package:angular_tour_of_heroes/app_component.dart';
 
 void main() {
-  bootstrap(AppComponent);
+  bootstrap(AppComponent, [
+    provide(BrowserClient, useFactory: () => new BrowserClient(), deps: [])
+  ]);
 }
