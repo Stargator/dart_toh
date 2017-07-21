0bb5fbd911e61de3ef0a587f0712eb96276bafe0
Stargator <Stargator@users.noreply.github.com>
2017-07-20 23:20:47 -0400

    Part 6 of Tutorial - HTTP - Before Updating Hero Details

--- lib/hero_service.dart
+++ lib/hero_service.dart
@@ -1,17 +1,52 @@
 import 'dart:async';
+import 'dart:convert';
+
 import 'package:angular2/angular2.dart';
+import 'package:http/http.dart';
 
 import 'hero.dart';
-import 'mock_heroes.dart';
 
 @Injectable()
 class HeroService {
-  Future<List<Hero>> getHeroes() async => mockHeroes;
+  static const _heroesUrl = "api/heroes"; // URL to web API
+  final Client _http;
+
+  HeroService(this._http);
+
+  Future<List<Hero>> getHeroes() async {
+    try {
+      final response = await _http.get(_heroesUrl);
+
+      final heroes = _extractData(response)
+          .map((value) => new Hero.fromJson(value))
+          .toList();
+
+      return heroes;
+    } catch (e) {
+      throw _handleError(e);
+    }
+  }
 
   Future<List<Hero>> getHeroesSlowly() {
     return new Future.delayed(const Duration(seconds: 2), getHeroes);
   }
 
-  Future<Hero> getHero(int id) async =>
-      (await getHeroes()).firstWhere((hero) => hero.id == id);
+  Future<Hero> getHero(int id) async {
+    try {
+      final response = await _http.get('$_heroesUrl/$id');
+
+      final hero = new Hero.fromJson(_extractData(response));
+
+      return hero;
+    } catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  dynamic _extractData(Response resp) => JSON.decode(resp.body)['data'];
+
+  Exception _handleError(dynamic e) {
+    print(e); // for demo purposes only
+    return new Exception("Server error; cause $e");
+  }
 }
