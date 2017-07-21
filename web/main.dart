99270d59fea0f25fbaa553c4dd2af8a7cc6572d5
Stargator <Stargator@users.noreply.github.com>
2017-07-20 23:20:47 -0400

    Part 6 of Tutorial - HTTP - Before Updating Hero Details

--- web/main.dart
+++ web/main.dart
@@ -3,12 +3,15 @@
 
 import 'package:angular2/angular2.dart';
 import 'package:angular2/platform/browser.dart';
-import 'package:http/browser_client.dart';
-
 import 'package:angular_tour_of_heroes/app_component.dart';
+import 'package:angular_tour_of_heroes/in_memory_data_service.dart';
+import 'package:http/http.dart';
 
 void main() {
   bootstrap(AppComponent, [
-    provide(BrowserClient, useFactory: () => new BrowserClient(), deps: [])
-  ]);
+    provide(Client, useClass: InMemoryDataService)
+  // Using a real back end?
+  // Import browser_client.dart and change the above to:
+  // [provide(Client, useFactory: () => new BrowserClient(), deps: [])]
+   ]);
 }
