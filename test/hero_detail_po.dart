d14569837f91644a0c05e3633596620339f8c5b3
Stargator <Stargator@users.noreply.github.com>
2017-07-20 23:49:37 -0400

    Part 6 of Tutorial - HTTP - Finish Add a hero service update() method

--- test/hero_detail_po.dart
+++ test/hero_detail_po.dart
@@ -19,8 +19,7 @@ class HeroDetailPO {
   PageLoaderElement _input;
 
   @ByTagName('button')
-  @optional
-  PageLoaderElement _button;
+  List<PageLoaderElement> _button;
 
   /// Retrieve information about hero
   Future<Map<String, dynamic>> get heroFromDetails async {
@@ -41,5 +40,5 @@ class HeroDetailPO {
   Future<dynamic> type(String s) => _input.type(s);
 
   /// Navigational function to go to the previous view
-  Future<dynamic> back() => _button.click();
+  Future<dynamic> back() => _button[0].click();
 }
